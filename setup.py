79ae258abc9d9a2e103bb7f320cab567a328be52
Anton-4 <anton.vanmoere@gmail.com>
2019-08-03 11:46:58 +0200

    added requirements to setup.py (#45)
    * added requirements to setup.py
    
    * Adding strip hints to the requirements
    
    Will be needed in the future once we have type hints in there

--- setup.py
+++ setup.py
@@ -5,8 +5,14 @@ from setuptools import find_packages
 with open("README.md") as stream:
     long_description = stream.read()
 
+REQUIREMENTS = [
+    'ascii-canvas>=1.2.2',
+    'ordereddict>=1.1',
+    'strip-hints>=0.1.7'
+]
+
 setup(name='flowpipe',
-      version='0.4.6',
+      version='0.4.7',
       author='Paul Schweizer',
       author_email='paulschweizer@gmx.net',
       description='Lightweight flow-based programming framework.',
@@ -14,12 +20,13 @@ setup(name='flowpipe',
       long_description_content_type='text/markdown',
       url='https://github.com/PaulSchweizer/flowpipe',
       packages=find_packages(),
+      install_requires=REQUIREMENTS,
       classifiers=[
               'Programming Language :: Python',
               'Programming Language :: Python :: 2.6',
               'Programming Language :: Python :: 2.7',
-              'Programming Language :: Python :: 3.3',
               'Programming Language :: Python :: 3.4',
               'Programming Language :: Python :: 3.5',
-              'Programming Language :: Python :: 3.6'
+              'Programming Language :: Python :: 3.6',
+              'Programming Language :: Python :: 3.7',
         ])
