b13fd033df5b655572e4ec8a11ede67e2457d565
Julian Schweizer <js@nect.com>
2019-08-15 11:00:54 +0200

    Amended tests to not rely on json odering

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -11,13 +11,15 @@ import flowpipe.utilities as util
 def test_node_encoder():
     """Test the custom JSONEncoder."""
     valid_object = {"key": "value", "other_key": [1, 2, 3]}
-    expected_json = '{"key": "value", "other_key": [1, 2, 3]}'
     json_string = json.dumps(valid_object)
-    assert json_string == expected_json
+    recovered_json = json.loads(json_string)
+    for k, v in valid_object.items():
+        assert v == recovered_json[k]
 
     invalid_object = {"key": "value", "other_key": bytes(42)}
     with pytest.raises(TypeError):
         json.dumps(invalid_object)
     json_string = json.dumps(invalid_object, cls=util.NodeEncoder)
-    expected_json = '{"key": "value", "other_key": "094c4931fdb2f2af417c9e0322a9716006e8211fe9017f671ac6e3251300acca"}'
-    assert json_string == expected_json
+    recovered_json = json.loads(json_string)
+    for k, v in invalid_object.items():
+        assert v == recovered_json[k] or sha256(v).hexdigest() == recovered_json[k]
