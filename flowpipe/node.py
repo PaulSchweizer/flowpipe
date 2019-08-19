e3112b603077a6ca06ac5ff234161b982631856f
Julian Schweizer <neuneck@gmail.com>
2019-08-19 10:15:03 +0200

    Added a custom JSONEncoder to allow for generic node values. (#60)
    * Added a custom JSONEncoder to allow for node values that the default JSONEncoder cannot handle:
      * If the JSONEncoder fails to encode, we try to get a sha256, if that fails, we fall back to the string representation of the object in question.

--- flowpipe/node.py
+++ flowpipe/node.py
@@ -21,7 +21,7 @@ import warnings
 from .plug import OutputPlug, InputPlug, SubInputPlug, SubOutputPlug
 from .log_observer import LogObserver
 from .stats_reporter import StatsReporter
-from .utilities import deserialize_node
+from .utilities import deserialize_node, NodeEncoder
 from .graph import get_default_graph
 __all__ = ['INode']
 
@@ -119,7 +119,8 @@ class INode(object):
         LogObserver.push_message(
             'Evaluating {0} -> {1}.compute(**{2})'.format(
                 self.file_location, self.class_name,
-                json.dumps(self._sort_plugs(inputs), indent=2)))
+                json.dumps(self._sort_plugs(inputs), indent=2, cls=NodeEncoder)
+            ))
 
         # Compute and redirect the output to the output plugs
         start_time = time.time()
@@ -149,7 +150,8 @@ class INode(object):
         LogObserver.push_message(
             'Evaluation result for {0} -> {1}: {2}'.format(
                 self.file_location, self.class_name,
-                json.dumps(self._sort_plugs(outputs), indent=2)))
+                json.dumps(self._sort_plugs(outputs), indent=2, cls=NodeEncoder)
+            ))
 
         return outputs
 
@@ -261,7 +263,7 @@ class INode(object):
 
             value = ""
             if in_plug.value is not None:
-                value = json.dumps(in_plug.value)
+                value = json.dumps(in_plug.value, cls=NodeEncoder)
             plug = '{symbol} {dist}{input_}{value}'.format(
                 symbol='%' if in_plug._sub_plugs else 'o',
                 dist=' ' if isinstance(in_plug, SubInputPlug)else '',
@@ -316,7 +318,7 @@ class INode(object):
                 pretty.append('{indent}[i] {name}: {value}'.format(
                     indent='   ' if isinstance(plug, SubInputPlug) else '  ',
                     name=name,
-                    value=json.dumps(plug.value)))
+                    value=json.dumps(plug.value, cls=NodeEncoder)))
         for name, plug in sorted(self.all_outputs().items()):
             if plug._sub_plugs:
                 pretty.append('  [o] {name}'.format(name=name))
@@ -332,7 +334,7 @@ class INode(object):
                 pretty.append('{indent}[o] {name}: {value}'.format(
                     indent='   ' if isinstance(plug, SubOutputPlug) else '  ',
                     name=name,
-                    value=json.dumps(plug.value)))
+                    value=json.dumps(plug.value, cls=NodeEncoder)))
 
         return '\n'.join(pretty)
 
---
+++ tests/test_utilities.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+
+import pytest
+
+import json
+import re
+from hashlib import sha256
+
+import flowpipe.utilities as util
+
+
+class WeirdObject(object):
+    """An object that is not json serializable and has no bytes() interface."""
+
+    foo = "bar"
+
+
+def test_node_encoder():
+    """Test the custom JSONEncoder."""
+    valid_object = {"key": "value", "other_key": [1, 2, 3]}
+    json_string = json.dumps(valid_object)
+    recovered_json = json.loads(json_string)
+    for k, v in valid_object.items():
+        assert v == recovered_json[k]
+
+    bytes_object = {"key": "value", "other_key": bytes(42)}
+    json_string = json.dumps(bytes_object, cls=util.NodeEncoder)
+    recovered_json = json.loads(json_string)
+    for k, v in bytes_object.items():
+        assert v == recovered_json[k] \
+            or sha256(v).hexdigest() == recovered_json[k]
+
+    weird_object = {"key": "value", "other_key": WeirdObject()}
+    json_string = json.dumps(weird_object, cls=util.NodeEncoder)
+    recovered_json = json.loads(json_string)
+    for k, v in weird_object.items():
+        assert v == recovered_json[k] \
+            or re.search('WeirdObject object at', str(recovered_json[k])) \
+            or sha256(v).hexdigest() == recovered_json[k]
