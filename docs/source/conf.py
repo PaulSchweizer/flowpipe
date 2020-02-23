2cfa37991a2c1b01ac33cfe12d01a41c7d09ec2c
Paul Schweizer <paulschweizer@gmx.net>
2020-02-23 18:10:57 +0100

    Conf test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../..'))
 sys.setrecursionlimit(1500)
 
 # -- Project information -----------------------------------------------------
