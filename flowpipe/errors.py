e17d70cd45834f231a174b99b2f413e39fc379b9
Paul Schweizer <paulschweizer@gmx.net>
2023-12-08 16:50:48 +0100

    Add error message with better explanation for potential pickle error

--- flowpipe/errors.py
+++ flowpipe/errors.py
@@ -3,3 +3,7 @@
 
 class CycleError(Exception):
     """Raised when an action would result in a cycle in a graph."""
+
+
+class FlowpipeMultiprocessingError(Exception):
+    """Raised when a Node can not be pickled, most likely due to inputs not being picklable."""
