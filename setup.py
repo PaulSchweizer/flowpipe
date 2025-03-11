79f0e793809df7eb88515f90c4a6cc49f8283b18
Paul Schweizer <paulschweizer@gmx.net>
2025-03-11 12:27:00 +0100

    Bump version to v1.0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ REQUIREMENTS = [
 
 setup(
     name="flowpipe",
-    version="1.0.3",
+    version="1.0.4",
     author="Paul Schweizer",
     author_email="paulschweizer@gmx.net",
     description="Lightweight flow-based programming framework.",
