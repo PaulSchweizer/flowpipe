c4f2ab87c37b6f3746d9f549f34526d04fcca2e3
Jonas Sorgenfrei <34287983+jonassorgenfrei@users.noreply.github.com>
2025-07-24 14:41:21 +0200

    more complex example for the vfx rendering (#182)

--- examples/vfx_rendering.py
+++ examples/vfx_rendering.py
@@ -6,54 +6,75 @@
 - Create and render a slap comp
 - Convert the rendered slapcomp to a quicktime
 
-+---------------------------+          +----------------------+           +-------------------------+          +-----------------------+          +-----------------------+
-|      MayaRender0-10       |          |   CheckImages0-10    |           |     CreateSlapComp      |          |    NukeRender0-10     |          |       Quicktime       |
-|---------------------------|          |----------------------|           |-------------------------|          |-----------------------|          |-----------------------|
-o frames<[0, 1, 2, >        |     +--->o images<>             |           % images                  |          o frames<[0, 1, 2, >    |          % images                |
-o scene_file<"/scene/fo>    |     |    |               images o---------->o  images.0<>             |     +--->o scene_file<>          |     +--->o  images.0<>           |
-|                renderings o-----+    +----------------------+      |--->o  images.10<>            |   --+    |            renderings o-----+--->o  images.10<>          |
-+---------------------------+          +-----------------------+     |--->o  images.20<>            |     |    +-----------------------+     |--->o  images.20<>          |
-+---------------------------+          |   CheckImages10-20    |     |    o template<"nuke_temp>    |     |    +-----------------------+     |    |             quicktime o
-|      MayaRender10-20      |          |-----------------------|     |    |                slapcomp o---  |    |    NukeRender10-20    |     |    +-----------------------+
-|---------------------------|     +--->o images<>              |     |    +-------------------------+     |    |-----------------------|     |
-o frames<[10, 11, 1>        |     |    |                images o-----|    +-----------------------+       |    o frames<[10, 11, 1>    |     |
-o scene_file<"/scene/fo>    |     |    +-----------------------+     |    |    UpdateDatabase     |       +--->o scene_file<>          |     |
-|                renderings o-----+    +-----------------------+     |    |-----------------------|       |    |            renderings o-----+
-+---------------------------+          |   CheckImages20-30    |     |    o id_<123456>           |       |    +-----------------------+     |
-+---------------------------+          |-----------------------|     |    % images                |       |    +-----------------------+     |
-|      MayaRender20-30      |     +--->o images<>              |     +--->o  images.0<>           |       |    |    NukeRender20-30    |     |
-|---------------------------|     |    |                images o-----+--->o  images.10<>          |       |    |-----------------------|     |
-o frames<[20, 21, 2>        |     |    +-----------------------+     +--->o  images.20<>          |       |    o frames<[20, 21, 2>    |     |
-o scene_file<"/scene/fo>    |     |                                       |                status o       +--->o scene_file<>          |     |
-|                renderings o-----+                                       +-----------------------+            |            renderings o-----+
-+---------------------------+                                                                                  +-----------------------+
++------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+|                                                                                                                   Rendering                                                                                                                    |
+|------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| +-----Rendering-----+          +--------Rendering---------+          +-------Rendering--------+           +------Rendering-------+           +--------Rendering--------+          +-------Rendering-------+          +--------default--------+ |
+| |   CreateCamera    |          |   MayaSceneGeneration    |          |   HoudiniRender0-10    |           |   CheckImages0-10    |           |     CreateSlapComp      |          |    NukeRender0-10     |          |       Quicktime       | |
+| |-------------------|          |--------------------------|          |------------------------|           |----------------------|           |-------------------------|          |-----------------------|          |-----------------------| |
+| |     camera_file<> o--------->o camera_file<>            |          o frames<range(0...>     |      +--->o images<>             |           % images<>                |          o frames<range(0...>    |          % images<>              | |
+| +-------------------+          |             scene_file<> o--------->o scene_file<>           |      |    |             images<> o---------->o  images.0<>             |     +--->o scene_file<>          |     +--->o  images.0<>           | |
+|                                +--------------------------+     |    |           renderings<> o------+    +----------------------+      |--->o  images.10<>            |     |    |          renderings<> o-----+--->o  images.10<>          | |
+|                                                                 |    +------------------------+           +-------Rendering-------+     +--->o  images.20<>            |     |    +-----------------------+     +--->o  images.20<>          | |
+|                                                                 |    +--------Rendering--------+          |   CheckImages10-20    |     |    o template<nuke_te...>    |     |    +-------Rendering-------+     |    |           quicktime<> o |
+|                                                                 |    |   HoudiniRender10-20    |          |-----------------------|     |    |              slapcomp<> o-----+    |    NukeRender10-20    |     |    +-----------------------+ |
+|                                                                 |    |-------------------------|     +--->o images<>              |     |    +-------------------------+     |    |-----------------------|     |                              |
+|                                                                 |    o frames<range(1...>      |     |    |              images<> o-----|    +-------Rendering-------+       |    o frames<range(1...>    |     |                              |
+|                                                                 +--->o scene_file<>            |     |    +-----------------------+     |    |    UpdateDatabase     |       +--->o scene_file<>          |     |                              |
+|                                                                 |    |            renderings<> o-----+    +-------Rendering-------+     |    |-----------------------|       |    |          renderings<> o-----|                              |
+|                                                                 |    +-------------------------+          |   CheckImages20-30    |     |    o id_<123456>           |       |    +-----------------------+     |                              |
+|                                                                 |    +--------Rendering--------+          |-----------------------|     |    % images<>              |       |    +-------Rendering-------+     |                              |
+|                                                                 |    |   HoudiniRender20-30    |     +--->o images<>              |     |--->o  images.0<>           |       |    |    NukeRender20-30    |     |                              |
+|                                                                 |    |-------------------------|     |    |              images<> o-----+--->o  images.10<>          |       |    |-----------------------|     |                              |
+|                                                                 |    o frames<range(2...>      |     |    +-----------------------+     +--->o  images.20<>          |       |    o frames<range(2...>    |     |                              |
+|                                                                 +--->o scene_file<>            |     |                                       |              status<> o       +--->o scene_file<>          |     |                              |
+|                                                                      |            renderings<> o-----+                                       +-----------------------+            |          renderings<> o-----+                              |
+|                                                                      +-------------------------+                                                                                  +-----------------------+                                    |
++------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
 """
 
 from flowpipe import Graph, Node
 
 
-@Node(outputs=["renderings"], metadata={"interpreter": "maya"})
-def MayaRender(frames, scene_file):
+@Node(outputs=["camera_file"], metadata={"interpreter": "3dequalizer"})
+def CreateCamera():
+    """Creates a shot camera."""
+    return {"camera_file": "/abs/camera.abc"}
+
+
+@Node(outputs=["scene_file"], metadata={"interpreter": "maya"})
+def MayaSceneGeneration(camera_file):
+    """Creates a Maya scene file for rendering."""
+    return {"scene_file": "/usd/scene.usd"}
+
+
+@Node(outputs=["renderings"], metadata={"interpreter": "houdini"})
+def HoudiniRender(frames, scene_file):
+    """Creates a Houdini scene file for rendering."""
     return {"renderings": "/renderings/file.%04d.exr"}
 
 
 @Node(outputs=["images"])
 def CheckImages(images):
+    """Check if the images are valid and return them."""
     return {"images": images}
 
 
-@Node(outputs=["slapcomp"])
+@Node(outputs=["slapcomp"], metadata={"interpreter": "nuke"})
 def CreateSlapComp(images, template):
+    """Create a nuke slapcomp scene file from the given images and template."""
     return {"slapcomp": "slapcomp.nk"}
 
 
 @Node(outputs=["renderings"], metadata={"interpreter": "nuke"})
 def NukeRender(frames, scene_file):
+    """Renders the slapcomp scene file using Nuke."""
     return {"renderings": "/renderings/file.%04d.exr"}
 
 
 @Node(outputs=["quicktime"])
 def Quicktime(images):
+    """Create a quicktime movie from the rendered images."""
     return {"quicktime": "resulting.mov"}
 
 
@@ -62,24 +83,33 @@ def UpdateDatabase(id_, images):
     """Update the database entries of the given asset with the given data."""
     return {"status": True}
 
-
 def complex_cg_render(frames, batch_size):
     graph = Graph(name="Rendering")
 
     slapcomp = CreateSlapComp(graph=graph, template="nuke_template.nk")
     update_database = UpdateDatabase(graph=graph, id_=123456)
+    
+    camera_creation = CreateCamera(graph=graph)
+    scene_creation = MayaSceneGeneration(graph=graph)
+
+    camera_creation.outputs["camera_file"].connect(
+        scene_creation.inputs["camera_file"]
+    )
 
     for i in range(0, frames, batch_size):
-        maya_render = MayaRender(
-            name="MayaRender{0}-{1}".format(i, i + batch_size),
+        houdini_render = HoudiniRender(
+            name="HoudiniRender{0}-{1}".format(i, i + batch_size),
             graph=graph,
             frames=range(i, i + batch_size),
             scene_file="/scene/for/rendering.ma",
         )
+        scene_creation.outputs["scene_file"].connect(
+            houdini_render.inputs["scene_file"]
+        )
         check_images = CheckImages(
             name="CheckImages{0}-{1}".format(i, i + batch_size), graph=graph
         )
-        maya_render.outputs["renderings"].connect(
+        houdini_render.outputs["renderings"].connect(
             check_images.inputs["images"]
         )
         check_images.outputs["images"].connect(
